f0dc03b7a03aea87a204013871ef87fb5892e9f7
Cristhian Selbach <crsthian.04@gmail.com>
2022-10-21 15:31:35 -0300

    navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,17 +1,29 @@
 <template>
-	<nav class="flex items-end justify-between mx-40 my-8">
+	<nav class="flex items-baseline mx-44 my-8">
 		<!-- Logo -->
-		<div
-			class="p-0.5 text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-lightGreen to-darkGreen select-none"
-		>
-			VeranceNews.
+		<div class="w-1/2 select-none">
+			<h1
+				class="cursor-pointer w-fit p-0.5 text-[2.5rem] font-bold text-transparent bg-clip-text bg-gradient-to-r from-lightGreen to-darkGreen"
+			>
+				VeranceNews.
+			</h1>
 		</div>
+
 		<!-- Items -->
-		<div class="pb-1 font-bold text-xl">
+		<div
+			class="w-1/2 font-bold text-[1.1rem] flex justify-between items-center"
+		>
 			<NuxtLink to="/"> HOME</NuxtLink>
-			<NuxtLink to="/"> ABOUT </NuxtLink>
 			<NuxtLink to="/"> CATEGORIES </NuxtLink>
-			<NuxtLink to="/"> GET STARTED</NuxtLink>
+			<NuxtLink to="/"> ABOUT </NuxtLink>
+
+			<!-- Navbar button -->
+			<NuxtLink
+				class="leading-3 bg-gradient-to-r from-lightGreen to-darkGreen tracking-tight text-white px-7 py-3 rounded-lg"
+				to="/"
+			>
+				GET START
+			</NuxtLink>
 		</div>
 	</nav>
 </template>
