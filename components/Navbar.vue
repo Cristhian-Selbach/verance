cca4071dfe73659b8dccdcf9d3686d50120f13b4
Cristhian Selbach <crsthian.04@gmail.com>
2022-10-24 18:28:30 -0300

    navbar button hover

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,24 +2,25 @@
 	<nav class="flex items-baseline mx-44 my-8">
 		<!-- Logo -->
 		<div class="w-1/2 select-none">
-			<h1
+			<NuxtLink
+				to="/"
 				class="cursor-pointer w-fit p-0.5 text-[2.5rem] font-bold text-transparent bg-clip-text bg-gradient-to-r from-lightGreen to-darkGreen"
 			>
 				VeranceNews.
-			</h1>
+			</NuxtLink>
 		</div>
 
 		<!-- Items -->
 		<div
 			class="w-1/2 font-bold text-[1.1rem] flex justify-between items-center"
 		>
-			<NuxtLink to="/"> HOME</NuxtLink>
-			<NuxtLink to="/"> CATEGORIES </NuxtLink>
-			<NuxtLink to="/"> ABOUT </NuxtLink>
+			<NuxtLink to="/" class="hover:text-stone-600"> HOME</NuxtLink>
+			<NuxtLink to="/" class="hover:text-stone-600">CATEGORIES</NuxtLink>
+			<NuxtLink to="/" class="hover:text-stone-600"> ABOUT </NuxtLink>
 
 			<!-- Navbar button -->
 			<NuxtLink
-				class="leading-3 bg-gradient-to-r from-lightGreen to-darkGreen tracking-tight text-white px-7 py-3 rounded-lg"
+				class="whitespace-nowrap leading-3 bg-gradient-to-r from-lightGreen to-darkGreen tracking-tight text-white px-7 py-3 rounded-lg hover:scale-105 ease-out duration-300"
 				to="/"
 			>
 				GET START
