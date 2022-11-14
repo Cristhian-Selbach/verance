c3d6f7aabd622ae110926c91c3c80bbfb1087e9e
Cristhian Selbach <crsthian.04@gmail.com>
2022-11-14 10:31:28 -0300

    all news with image

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,17 @@
 import * as dotenv from "dotenv";
 import { Router } from "express";
 import axios from "axios";
+import { promises } from "stream";
 
 dotenv.config();
 const router = Router();
 
 function baseUrl(category: string) {
-	return `https://newsdata.io/api/1/news?apikey=${process.env.API_KEY}&language=en&category=${category}`;
+	return `https://newsdata.io/api/1/news?apikey=${process.env.NEWS_API_KEY}&language=en&category=${category}`;
+}
+
+function getImage(query: string) {
+	return `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${process.env.IMAGES_API_KEY}`;
 }
 
 router.get("/", async (req, res) => {
@@ -17,9 +22,19 @@ router.get("/", async (req, res) => {
 			res.status(400).send("error in api call");
 		}
 
+		await Promise.all(
+			data.results.map(async (element) => {
+				if (element.image_url) return;
+
+				const { data } = await axios.get(getImage(element.category[0]));
+				const random = Math.floor(Math.random() * 8);
+				element.image_url = await data.results[random].urls.small;
+			})
+		);
+
 		res.json(data);
 	} catch (error) {
-		res.status(400).send("error in api call");
+		res.status(400).send("error in api call " + error);
 	}
 });
 
