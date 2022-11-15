58d7d056edb2c14faf4a2efe86542d00deadc659
Cristhian Selbach <crsthian.04@gmail.com>
2022-11-15 12:22:56 -0300

    card design

--- client/pages/news.vue
+++ client/pages/news.vue
@@ -7,32 +7,57 @@
 		return dayjs(date).format("lll");
 	}
 
-	const { data } = await useFetch("http://localhost:8080/");
-	const news = data._rawValue;
+	let news: Array<any>;
 
-	const firstNews = news[0];
-	const secondNews = news[1];
+	try {
+		const { data } = await useFetch("http://localhost:8080/");
+		news = data.value;
+	} catch (e) {
+		console.log("error");
+	}
 </script>
 
 <template>
-	<section class="mx-8 sm:mx-24 lg:mx-44">
+	<section class="mx-8 mt-20 sm:mx-24 lg:mx-44">
 		<div class="flex w-full">
-			<NuxtLink to="/news" class="shadow rounded-[1.2rem] w-2/3 p-5">
-				<p>Technology in General</p>
-				<div class="flex">
-					<img class="w-1/2" :src="firstNews.urlToImage" alt="" />
-					<h1>{{ firstNews.title }}</h1>
+			<NuxtLink
+				to="/news"
+				class="h-[530px] shadow rounded-[1.2rem] w-2/3 p-5"
+			>
+				<p class="uppercase text-zinc-400 font-semibold text-sm">
+					{{ news[0].category[0] }} IN GENERAL
+				</p>
+				<div class="flex mt-3 h-1/2">
+					<div class="h-full w-1/2">
+						<img
+							class="rounded-lg mr-2 object-cover w-full h-full"
+							:src="news[0].image_url"
+							alt=""
+						/>
+					</div>
+
+					<h1
+						class="font-bold w-1/2 text-[2.2rem] leading-[2.5rem] ml-2"
+					>
+						{{ news[0].title }}
+					</h1>
 				</div>
-				<p>{{ firstNews.description }}</p>
-				<p>{{ firstNews.content }}</p>
-				<p v-if="news.author">
-					By {{ firstNews.author }}
-					{{ format(firstNews.publishedAt) }}
-				</p>
-				<p v-else>
-					By {{ firstNews.source.name }}
-					{{ format(firstNews.publishedAt) }}
-				</p>
+				<div
+					class="text-[#959595] h-1/2 flex flex-col justify-around texts font-bold"
+				>
+					<p class="tracking-tighter">
+						{{ news[0].description }}<br />
+						{{ news[0].content.slice(0, 400) }}...
+					</p>
+
+					<p
+						v-if="news[0].creator"
+						class="tracking-tighter font-semibold"
+					>
+						By {{ news[0].creator[0] }} at
+						{{ format(news[2].pubDate) }}
+					</p>
+				</div>
 			</NuxtLink>
 		</div>
 	</section>
@@ -40,8 +65,8 @@
 
 <style scoped>
 	.shadow {
-		-webkit-box-shadow: 4px 4px 9px 0px rgba(0, 0, 0, 0.1);
-		-moz-box-shadow: 4px 4px 9px 0px rgba(0, 0, 0, 0.1);
-		box-shadow: 4px 4px 9px 0px rgba(0, 0, 0, 0.1);
+		-webkit-box-shadow: 3px 3px 9px 0px rgba(0, 0, 0, 0.2);
+		-moz-box-shadow: 3px 3px 9px 0px rgba(0, 0, 0, 0.2);
+		box-shadow: 3px 3px 9px 0px rgba(0, 0, 0, 0.2);
 	}
 </style>
